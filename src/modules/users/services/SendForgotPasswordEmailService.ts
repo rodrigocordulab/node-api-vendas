f780d54c1ac21b21f0b7294653db518d694f1353
Rodrigo <rodrigo.cordula@gmail.com>
2022-01-17 17:37:14 -0300

    Implement EtherealFakeEmail and Template Handlebars and Finished Reset Password with SendEmail

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,9 @@
 import AppError from '@shared/errors/AppErrors';
 import { getCustomRepository } from 'typeorm';
+import path from 'path';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import { UsersTokensRepository } from '../typeorm/repositories/UsersTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -18,10 +20,30 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
 
-    const token = await userTokensRepository.generate(user.id);
+    const { token } = await userTokensRepository.generate(user.id);
 
-    console.log(token);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
+    await EtherealMail.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
+    });
     // return user;
   }
 }
